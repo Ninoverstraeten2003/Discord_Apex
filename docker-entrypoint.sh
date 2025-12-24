9ceec69005169dba3f6e9e57bedcfc362e0793de
Nino Verstraeten <verstraeten.nino@gmail.com>
2025-12-24 01:05:25 +0100

    remove: redundancy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,9 @@ set -e
 
 echo "=== Entrypoint Script Started ==="
 echo "APP_DIR: ${APP_DIR}"
+echo "PLAYER_NAME: ${PLAYER_NAME}"
 
-# Map Railway variables to expected variable names based on APP_DIR
+# Map Railway variables to expected variable names based on APP_DIR and PLAYER_NAME
 if [ -z "$DISCORD_BOT_TOKEN" ]; then
   case "$APP_DIR" in
     apex)
@@ -16,50 +17,55 @@ if [ -z "$DISCORD_BOT_TOKEN" ]; then
         echo "WARNING: DISCORD_BOT_TOKEN_MAP not set"
       fi
       ;;
-    apex_daan)
-      echo "Mapping variables for apex_daan..."
-      if [ -n "$DISCORD_BOT_TOKEN_DAAN" ]; then
-        export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_DAAN"
-        echo "Mapped DISCORD_BOT_TOKEN_DAAN to DISCORD_BOT_TOKEN"
-      else
-        echo "WARNING: DISCORD_BOT_TOKEN_DAAN not set"
-      fi
-      if [ -n "$PLAYER_UID_DAAN" ]; then
-        export PLAYER_UID="$PLAYER_UID_DAAN"
-        echo "Mapped PLAYER_UID_DAAN to PLAYER_UID"
-      else
-        echo "WARNING: PLAYER_UID_DAAN not set"
-      fi
-      ;;
-    apex_eben)
-      echo "Mapping variables for apex_eben..."
-      if [ -n "$DISCORD_BOT_TOKEN_EBEN" ]; then
-        export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_EBEN"
-        echo "Mapped DISCORD_BOT_TOKEN_EBEN to DISCORD_BOT_TOKEN"
-      else
-        echo "WARNING: DISCORD_BOT_TOKEN_EBEN not set"
-      fi
-      if [ -n "$PLAYER_UID_EBEN" ]; then
-        export PLAYER_UID="$PLAYER_UID_EBEN"
-        echo "Mapped PLAYER_UID_EBEN to PLAYER_UID"
-      else
-        echo "WARNING: PLAYER_UID_EBEN not set"
-      fi
-      ;;
-    apex_nino)
-      echo "Mapping variables for apex_nino..."
-      if [ -n "$DISCORD_BOT_TOKEN_NINO" ]; then
-        export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_NINO"
-        echo "Mapped DISCORD_BOT_TOKEN_NINO to DISCORD_BOT_TOKEN"
-      else
-        echo "WARNING: DISCORD_BOT_TOKEN_NINO not set"
-      fi
-      if [ -n "$PLAYER_UID_NINO" ]; then
-        export PLAYER_UID="$PLAYER_UID_NINO"
-        echo "Mapped PLAYER_UID_NINO to PLAYER_UID"
-      else
-        echo "WARNING: PLAYER_UID_NINO not set"
-      fi
+    apex_player)
+      echo "Mapping variables for apex_player (player bot: ${PLAYER_NAME})..."
+      case "$PLAYER_NAME" in
+        DAAN)
+          if [ -n "$DISCORD_BOT_TOKEN_DAAN" ]; then
+            export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_DAAN"
+            echo "Mapped DISCORD_BOT_TOKEN_DAAN to DISCORD_BOT_TOKEN"
+          else
+            echo "WARNING: DISCORD_BOT_TOKEN_DAAN not set"
+          fi
+          if [ -n "$PLAYER_UID_DAAN" ]; then
+            export PLAYER_UID="$PLAYER_UID_DAAN"
+            echo "Mapped PLAYER_UID_DAAN to PLAYER_UID"
+          else
+            echo "WARNING: PLAYER_UID_DAAN not set"
+          fi
+          ;;
+        EBEN)
+          if [ -n "$DISCORD_BOT_TOKEN_EBEN" ]; then
+            export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_EBEN"
+            echo "Mapped DISCORD_BOT_TOKEN_EBEN to DISCORD_BOT_TOKEN"
+          else
+            echo "WARNING: DISCORD_BOT_TOKEN_EBEN not set"
+          fi
+          if [ -n "$PLAYER_UID_EBEN" ]; then
+            export PLAYER_UID="$PLAYER_UID_EBEN"
+            echo "Mapped PLAYER_UID_EBEN to PLAYER_UID"
+          else
+            echo "WARNING: PLAYER_UID_EBEN not set"
+          fi
+          ;;
+        NINO)
+          if [ -n "$DISCORD_BOT_TOKEN_NINO" ]; then
+            export DISCORD_BOT_TOKEN="$DISCORD_BOT_TOKEN_NINO"
+            echo "Mapped DISCORD_BOT_TOKEN_NINO to DISCORD_BOT_TOKEN"
+          else
+            echo "WARNING: DISCORD_BOT_TOKEN_NINO not set"
+          fi
+          if [ -n "$PLAYER_UID_NINO" ]; then
+            export PLAYER_UID="$PLAYER_UID_NINO"
+            echo "Mapped PLAYER_UID_NINO to PLAYER_UID"
+          else
+            echo "WARNING: PLAYER_UID_NINO not set"
+          fi
+          ;;
+        *)
+          echo "WARNING: Unknown PLAYER_NAME: ${PLAYER_NAME}"
+          ;;
+      esac
       ;;
     *)
       echo "WARNING: Unknown APP_DIR: ${APP_DIR}"
@@ -79,4 +85,3 @@ fi
 
 echo "=== Starting Application ==="
 exec "$@"
-
